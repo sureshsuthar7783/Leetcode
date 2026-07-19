153b1ce675895dc82402742b8f3504ea068afe06
sureshsuthar7773 <ssureshsuthar007>
2026-07-19 23:24:25 +0530

    array beginner

---
+++ arrrayBasic.js
@@ -0,0 +1,107 @@
+// let arr = [ 1,2,3,455 ,-1 ,45,,-2,4,]
+
+// // for(let i = 0; i<arr.length;i++){
+// //     console.log(arr[i])
+// }
+
+// const  n = 5;
+
+// let count = 0;
+
+
+// for(let i = 0; i<n;i++){
+//     count++;
+// }
+// console.log(count)
+
+
+// find the som of the array
+// let sum  = 0;
+// for(let i = 0 ; i<arr.length;i++){
+//     sum  = sum + arr[i]
+    
+// }
+
+
+// console.log(sum)
+
+// find the factorial of number 
+
+
+
+// find the avg of the array
+
+// let sum = 0;
+// for ( let i=0; i<arr.length;i++){
+//     sum  = sum + arr[i];
+
+// }
+//  console.log(sum / arr.length)
+//  console.log(arr.length)
+
+
+// find the largest element in the array
+// let largest = arr[0]
+// for(let i = 0 ; i<arr.length; i++){
+
+//     if (arr[i]>largest){
+//     largest  = arr[i]
+//     }
+// }
+// console.log(largest)
+
+
+
+
+//find the smallest number in the array
+// let smallest = arr[0]
+
+// for (let i = 0; i<arr.length;i++){
+//     if (arr[i]<smallest){
+//         smallest = arr[i]
+//     }
+// }
+// console.log(smallest)
+
+
+// find  the even element 
+
+// for (let i = 0 ; i<arr.length;i++){
+//     if(arr[i]%2 ==0 ){
+//         console.log(" this is even number",arr[i])
+//     }
+//     else{
+//         console.log("this is odd number",arr[i])
+//     }
+// }
+
+
+// count the even 
+// let countEven = 0;
+
+// for(let i = 0 ; i<arr.length;i++){
+//     if(arr[i]%2 ==0){
+//         countEven++;
+
+//     }
+
+    
+
+// }
+// console.log(countEven)
+
+// // count the odd no
+// let countEven = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 == 1) {
+//         console.log(arr[i])
+//         countEven++;
+
+//     }
+
+
+
+// }
+// console.log(countEven)
+
